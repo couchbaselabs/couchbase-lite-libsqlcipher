0a883c39240a48feafae54daa32c09e4cc1aca10
Pasin Suriyentrakorn <pasin@couchbase.com>
2015-11-02 15:35:28 -0800

    Add sqlite3.h to libs/include

--- build-amalgamation.sh
+++ build-amalgamation.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# Output directory for public headers
+OUTPUT_DIR="`pwd`/libs/include"
+if [ -d $OUTPUT_DIR ]; then
+    rm -rf $OUTPUT_DIR
+fi
+mkdir $OUTPUT_DIR
+
 cd vendor/sqlcipher
 
 # Clean:
@@ -16,6 +23,9 @@ rm -f ../../src/c/sqlite3.c
 rm -f ../../src/headers/sqlite3.h
 cp sqlite3.c ../../src/c/
 cp sqlite3.h ../../src/headers/
+
+# Public headers:
+cp sqlite3.h $OUTPUT_DIR
 
 # Clean and exit:
 git clean -dfx && git checkout -f
